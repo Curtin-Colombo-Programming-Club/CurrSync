9a25314b22698a0bfb0ab10d9823f61032e00fb6
Todo Lodo <me@todolodo.xyz>
2024-01-10 00:21:23 +0530

    Added __main__.py with function declarations and foundational structure

---
+++ __main__.py
@@ -0,0 +1,21 @@
+# imports
+
+
+def getReq():
+    """ 
+    GET request data from currency exchange api endpoint
+    """
+    
+
+def pushNotification():
+    """ 
+    push notification from programm
+    """
+    
+
+def main():
+    ...
+    
+    
+if __name__ == "__main__":
+    main()
